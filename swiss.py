00fadb94630a46238b67b3f2b9580defd7b30f09
BigfufuOuO <923525301@qq.com>
2024-10-07 17:13:39 +0800

    无

--- swiss.py
+++ swiss.py
@@ -1,47 +1,10 @@
-import random
+import requests
 
-list2_1 = {
-    "DK": ["FNC", "FLY", "LNG"],
-    "FLY": ["MAD", "DK", "PSG"],
-    "G2": ["PNG", "HLE", "WBG"],
-    "HLE": ["PSG", "G2", "GEN"],
-    "T1": ["TES", "PNG", "BLG"],
-    "TES": ["T1", "GEN", "FNC"],
-}
+def get_html(url):
+    headers = {
+        'authorization': '7935be4c41d8760a28c05581a7b1f570',
+    }
+    response = requests.get(url, headers=headers)
+    return response.text
 
-list1_2 = {
-    "BLG": ["MAD", "LNG", "T1"],
-    "FNC": ["DK", "GAM", "TES"],
-    "PSG": ["HLE", "MAD", "FLY"],
-    "WBG": ["GEN", 'TL', 'G2'],
-}
-
-list0_2 = {
-    'GAM': ['FLY', 'FNC', 'MAD'],
-    'MAD': ['BLG', 'PSG', 'GAM'],
-    'PNG': ['G2', 'T1', 'TL'],
-    'TL': ['LNG', 'WBG', 'PNG'],
-}
-    
-# 0-2交战
-battle0_2 = [['GAM', 'MAD'], ['PNG', 'TL']]
-
-iter = 0
-while(iter<1):
-    # 0-2交战
-    for battle in battle0_2:
-        team1 = battle[0]
-        team2 = battle[1]
-        if random.randint(0, 1) == 0:
-            list1_2[team1] = list0_2[team1]
-        else:
-            list1_2[team2] = list0_2[team2]
-    
-    battle2_1 = [[0, 0]]
-    # 2-1交战，随机选2个组成一组
-    team1, team2 = random.sample(list2_1.keys(), 2)
-    battle2_1.append([team1, team2])
-    print(battle2_1)
-        
-            
-    iter += 1
+print(get_html('https://open.tjstats.com/match-auth-app/open/v1/compound/matchDetail?matchId=11444'))
